f74465f955e3f3ed6c7df0556786ca83d5c43b4b
wilco menge <wilcomenge@gmail.com>
2020-05-30 17:27:36 +0200

    add view and controller

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,10 @@
 sap.ui.define([
-	"sap/m/Button",
-	"sap/m/MessageToast"
-], function (Button, MessageToast) {
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
-	new Button({
-		text: "Ready...",
-		press: function () {
-			MessageToast.show("Hello World!");
-		}
-	}).placeAt("content");
+	XMLView.create({viewName: "Ui5Test.App"}).then(function (oView) {
+		oView.placeAt("content");
+	});
 
 });
