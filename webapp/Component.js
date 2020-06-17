80705bac2a1d925de8a51364d5582149b59977c0
wilco menge <wilcomenge@gmail.com>
2020-06-17 08:26:58 +0200

    code cleanup

--- webapp/Component.js
+++ webapp/Component.js
@@ -7,22 +7,6 @@ sap.ui.define([
    return UIComponent.extend("Ui5Test.Component", {
       metadata: {
          manifest: "json"
-      },
-      init : function () {
-         // call the init function of the parent
-         UIComponent.prototype.init.apply(this, arguments);
-         // set data model
-         this.setModel(new JSONModel({
-					features: [
-						"Enterprise-Ready Web Toolkit",
-						"Powerful Development Concepts",
-						"Feature-Rich UI Controls",
-						"Consistent User Experience",
-						"Free and Open Source",
-						"Responsive Across Browsers and Devices",
-					]
-				})
-			);
       }
    });
 });
